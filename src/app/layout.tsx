98713e974de630ea1ef4722ac641527fb6cd3d6a
savka777 <sav.bojko@gmail.com>
2024-05-28 17:27:34 +0100

    landing page created

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { cn } from '@/lib/utils'
+
 import "./globals.css";
+import type { Metadata } from "next";
+import './globals.css'
+import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +18,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en' className='light'>
+        <body
+          className={cn(
+            'min-h-screen font-sans antialiased grainy',
+            inter.className
+          )}>
+          {children}
+        </body>
     </html>
-  );
+  )
 }
