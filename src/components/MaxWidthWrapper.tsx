b3b862fffa2a16325d14db88c88dc071c4d114f7
savka777 <sav.bojko@gmail.com>
2024-05-28 20:19:58 +0100

    Updated header and Kinde integration for SignUp/In

---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,21 @@
+'use client'; // Ensure that the componenet is treated as a client component
+import { cn } from '@/lib/utils'
+import { ReactNode } from 'react'
+
+const MaxWidthWrapper = ({
+  //An optional string that allows you to pass additional CSS classes to customize the styling of the div.
+  className,
+  //The content to be rendered inside the MaxWidthWrapper. This could be any valid React nodes, such as elements, text, or other components.
+  children,
+}: {
+  className?: string
+  children: ReactNode
+}) => {
+  return (
+    <div className={cn('mx-auto w-full max-w-screen-xl px-2.5 md:px-20', className)}>
+      {children}
+    </div>
+  )
+}
+
+export default MaxWidthWrapper
